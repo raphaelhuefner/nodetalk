ace1789aea274cda62b7c2e228350950ad8c87d8
Raphael Huefner <raphael@affinitybridge.com>
2011-04-05 03:14:13 -0700

    added PHP variant

---
+++ mysql.php
@@ -0,0 +1,24 @@
+<?php
+
+header('content-type: text/plain', TRUE, 200);
+echo "hello\n";
+
+try {
+  $db = new PDO('mysql:host=127.0.0.1;port=8889', 'dev', 'dev')
+} catch (Exception $err) {
+  echo 'connect problem';
+  throw $err;
+}
+
+try {
+  $dbStatement = $db->prepare("SELECT SLEEP(?) AS LEEP, ? AS morning_after;");
+  $dbStatement->execute(array(0.666, 'Rise and shine!'));
+  $results = $dbStatement->fetchAll(PDO::FETCH_ASSOC);
+} catch (Exception $err) {
+  echo 'connect problem';
+  throw $err;
+}
+
+echo json_encode($results) . "\n";
+
+echo "world!\n";
